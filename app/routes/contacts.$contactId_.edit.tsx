24199ef35ca25dcb487ab55a66c81346482ef2e3
y-uchiida <y.uchiida@nirneege.com>
2025-01-03 20:35:46 +0900

    feat: :sparkles: contact の編集画面のキャンセルボタンの挙動を実装

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -5,7 +5,7 @@ import type {
 	LoaderFunctionArgs
 } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData, useNavigate } from "@remix-run/react";
 import invariant from "tiny-invariant";
 
 import { getContact, updateContact } from "../data";
@@ -41,6 +41,7 @@ export const action = async ({ params, request }: ActionFunctionArgs) => {
 
 export default function EditContact() {
 	const { contact } = useLoaderData<typeof loader>();
+	const navigate = useNavigate();
 
 	return (
 		<Form key={contact.id} id="contact-form" method="post">
@@ -90,7 +91,10 @@ export default function EditContact() {
 			</label>
 			<p>
 				<button type="submit">Save</button>
-				<button type="button">Cancel</button>
+				{/* navigate(-1) で、ページの履歴のひとつ前のページを表示する */}
+				<button onClick={() => navigate(-1)} type="button">
+					Cancel
+				</button>
 			</p>
 		</Form>
 	);
